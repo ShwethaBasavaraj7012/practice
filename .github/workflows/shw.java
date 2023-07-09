7f12882baba298bd17cf0699e67fa188edf7b63f
ShwethaBasavaraj7012 <134492892+ShwethaBasavaraj7012@users.noreply.github.com>
2023-07-09 20:25:15 +0530

    Update shw.java

--- .github/workflows/shw.java
+++ .github/workflows/shw.java
@@ -1,4 +1,12 @@
-class shwetha
-  {
-    System.out.println("My name is shwetha");
+class Main {
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
   }
+}
